defa05d0966a36cf1cff5e44f1d1fc5e37aafdca
Tiến Đặng Anh <20520800@ms.uit.edu.vn>
2022-09-23 10:03:19 +0700

    fix inference.py error

--- inference.py
+++ inference.py
@@ -20,11 +20,10 @@ map_dict = {
 
 if __name__ == "__main__":
     sentence = input("Enter a sentence: ")
-    seg_sentence = ViTokenizer.tokenize(sentence)
+    seg_sentence = ViTokenizer.tokenize(sentence.lower())
     tokens = tokenizer(seg_sentence, return_tensors='pt')
 
     with torch.no_grad():
         output = model(tokens["input_ids"].to(device), tokens["attention_mask"].to(device))
-        print(torch.argmax(output, dim=1).item())
         print(f">> RESULT: {map_dict.get(int(torch.argmax(output, dim=1).item()), -1)}")
     print("Done...")
