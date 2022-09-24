d60e4bfaa38c22b6fd3f005b7f6fad829cad0b1d
Tiến Đặng Anh <20520800@ms.uit.edu.vn>
2022-09-24 14:21:27 +0700

    update inference.py

--- inference.py
+++ inference.py
@@ -29,10 +29,11 @@ if __name__ == "__main__":
         with torch.no_grad():
             output = model(tokens["input_ids"].to(device), tokens["attention_mask"].to(device))
             pred_label = int(torch.argmax(output, dim=1).item())
+            accuracy = round(output[0][pred_label].item(), 3)
             if pred_label == 2:
-                print("\tResult: "+ colored(map_dict[pred_label], "blue") + ", Accuracy: " + colored(output[0][pred_label].item(), "blue") + "\n")
+                print("\tResult: "+ colored(map_dict[pred_label], "blue") + ", Accuracy: " + colored(accuracy, "blue") + "\n")
             elif pred_label == 1:
-                print("\tResult: "+ colored(map_dict[pred_label], "yellow") + ", Accuracy: " + colored(output[0][pred_label].item(), "yellow") + "\n")
+                print("\tResult: "+ colored(map_dict[pred_label], "yellow") + ", Accuracy: " + colored(accuracy, "yellow") + "\n")
             else:
-                print("\tResult: "+ colored(map_dict[pred_label], "red") + ", Accuracy: " + colored(output[0][pred_label].item(), "red") + "\n")
+                print("\tResult: "+ colored(map_dict[pred_label], "red") + ", Accuracy: " + colored(accuracy, "red") + "\n")
     print(colored("Done...", "green"))
