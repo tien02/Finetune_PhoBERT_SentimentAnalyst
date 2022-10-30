e122ee0dd7cb367373b68c7acfe47b4cd9f94f7b
Tien Dang <tiendang@Tiens-MacBook-Pro.local>
2022-10-31 01:26:43 +0700

    update config

--- config/train_config.py
+++ config/train_config.py
@@ -1,19 +1,43 @@
-CHECKPOINT = "vinai/phobert-base"
-DATA_PATH = "/Users/tiendang/Documents/Work/Project/Research/nlp/cs221_sentanalyst/uit_vsfc_data/train.csv"
+import os
 
+# PRETRAINED MODEL
+CHECKPOINT = "vinai/phobert-base"
+
+# DATA
+TRAIN_PATH = "../uit_vsfc_data/train.csv"
+VALIDATION_PATH = "../uit_vsfc_data/dev.csv"
+TEST_PATH = "../uit_vsfc_data/test.csv"
+
+# DATALOADER
+LABEL = "sentiments"
+NUM_WORKERS = 3
+
+# TRAINER
 EPOCHS = 3
-VAL_EACH_EPOCH = 2
+VAL_EACH_EPOCH = 1
 NUM_CLASSES = 3
 BATCH_SIZE = 64
 THRESHOLD=0.5
+LEARNING_RATE = 1e-4
+ACCELERATOR = "gpu"
 
+# MODEL
+SEED = 42
 MODEL = "FeedForward-base"   # "FeedForward"/"LSTM" + '-' +  'base'/'large'
+DROP_OUT = 0.4
 
-LOGGER = {
-  "name": "PhoBERT_base",
-  "version": 0,
+# TENSORBOARD LOG
+TENSORBOARD = {
+    "DIR": "LOG",
+    "NAME": f"{MODEL}_DROP{DROP_OUT}_LABEL{LABEL}_LR{LEARNING_RATE}",
+    "VERSION": "0",
 }
 
-CKPT_DIR = "./checkpoint"
-CKPT_PATH =  "checkpoint/epoch=23-step=4296.ckpt"  # Checkpoint for inference, default is None
-CKPT_RESUME_TRAIN = None    # Checkpoint for continue training, default is None
+# CHECKPOINT
+CHECKPOINT_DIR = os.path.join(TENSORBOARD["DIR"], TENSORBOARD["NAME"], TENSORBOARD["VERSION"], "CKPT")
+
+# EVALUATE
+TEST_CKPT_PATH = ""
+
+# KEEP_TRAINING PATH
+CONTINUE_TRAINING = None
